e890a5c328f888cc5ee1c51e589caabc86deec63
margaret254 <maggiemwas91@gmail.com>
2020-01-15 11:04:37 +0300

    Add all the necessary files

--- personal/views.py
+++ personal/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from operator import attrgetter
+
+from award.views import get_award_queryset
 from award.models import AwardPost
 
 # Create your views here.
@@ -7,7 +9,13 @@ from award.models import AwardPost
 def home_screen_view(request):
 
     context = { }
-    award_posts = sorted(AwardPost.objects.all(), key=attrgetter('date_updated'), reverse=True)
+
+    query = ""
+    if request.GET:
+        query = request.GET['q']
+        context['query'] = str(query)
+
+    award_posts = sorted(get_award_queryset(query), key=attrgetter('date_updated'), reverse=True)
     context['award_posts'] = award_posts
     
     return render(request, "personal/home.html",context)
