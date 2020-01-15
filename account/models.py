e8672419186dc08fb37aabc4c4a6162841c15894
margaret254 <maggiemwas91@gmail.com>
2020-01-15 14:35:46 +0300

    Fix all the errors

--- account/models.py
+++ account/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
 # Create your models here.
 
 class MyAccountManager(BaseUserManager):
@@ -51,10 +56,13 @@ class Account(AbstractBaseUser):
 	def __str__(self):
 		return self.email
 
-	# For checking permissions. to keep it simple all admin have ALL permissons
 	def has_perm(self, perm, obj=None):
 		return self.is_admin
 
-	# Does this user have permission to view this app? (ALWAYS YES FOR SIMPLICITY)
 	def has_module_perms(self, app_label):
 		return True
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
